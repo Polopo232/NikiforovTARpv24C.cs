b641ebd113f25a569ece5a3ca0c4d48ce816da2b
Nikita Nikiforov <130934938+Polopo232@users.noreply.github.com>
2025-09-08 09:57:14 +0300

    Add files via upload

---
+++ EsimeneTund/Isik.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsimeneTund
+{
+    internal class Isik
+    {
+        public string eesNimi;
+        public string perNimi = "Tundumatu";
+        public int synniaasta = 2000;
+
+        public Isik() { }
+        public Isik(string eesNimi, string perNimi)
+        {
+            this.eesNimi = eesNimi;
+            this.perNimi = perNimi;
+        }
+        public void printInfo()
+        {
+            Console.WriteLine($"Isiku nimi on {eesNimi} {perNimi}");
+        }
+    }
+}
