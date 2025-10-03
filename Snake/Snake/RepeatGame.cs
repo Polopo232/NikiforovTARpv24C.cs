64e4af501ceb87a32c4a93218dcc270c4ce311c7
Ja <opilane@HP-CZC2349HTR>
2025-10-03 09:27:38 +0300

    RepeatGame Class

---
+++ Snake/Snake/RepeatGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EsimeneTund
+{
+    internal class RepeatGame
+    {
+        public bool Repeat()
+        {
+            Console.WriteLine("Kas soovite mängu uuesti alustada? (Y/N)");
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                if (keyInfo.Key == ConsoleKey.Y)
+                {
+                    return true;
+                }
+                else if (keyInfo.Key == ConsoleKey.N)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
